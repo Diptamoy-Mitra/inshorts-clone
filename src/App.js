21c9f621d0c60e1047013f99caf34fdd9587e6b2
diptamoy mitra <mitradiptamoy70@gmail.com>
2023-04-22 22:30:23 +0530

    edit proxy

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const newsApi= async () =>{
   try {
     const proxyUrl="https://cors-anywhere.herokuapp.com/";
     
-    const news=await axios.get(`https://newsapi.org/v2/top-headlines?country=in&apiKey=f88288f4bca14599a2334cadfb3d6f4b&pageSize=${loadMore}&category=${category}`);
+    const news=await axios.get(`https://${proxyUrl}newsapi.org/v2/top-headlines?country=in&apiKey=f88288f4bca14599a2334cadfb3d6f4b&pageSize=${loadMore}&category=${category}`);
 
   setNewsArray(news.data.articles)
   setNewsResults(news.data.totalResults)
